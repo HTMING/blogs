89c6ca4246115272f4184ab20ca92e56c562fa8e
TIJK <htming1993@gmail.com>
2018-09-04 23:46:32 +0800

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'content', 'pub')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('blog', 'name', 'content', 'pub')
---
+++ blog/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+"""
+借此完成博客的评论功能
+"""
